51fb480a3fedca03767f1e368fa6598208b9aa5f
Georgi Donev <72747971+DonevGeorgi@users.noreply.github.com>
2022-11-17 13:06:23 +0200

    Guess the number
    Adding guess the number game

---
+++ GuessANumber/GuessANumber/GuessANumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GuessANumber
+{
+    internal class GuessANumber
+    {
+        static void Main()
+        {
+            Random randomNumber = new Random();
+            int computerChosenNumber = randomNumber.Next(1, 101);
+
+            while (true)
+            {
+                Console.Write("Guess a number in range 1 to 100: ");
+
+                string playerInput = Console.ReadLine();
+
+                bool isNumber = int.TryParse(playerInput, out int playerGuess);
+
+                if (isNumber)
+                {
+                    if (playerGuess == computerChosenNumber)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                    if (playerGuess > computerChosenNumber)
+                    {
+                        Console.WriteLine("Number is too high.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Number is too low.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+        }
+    }
+}
